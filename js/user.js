92b4b2e479b4dabd2593a80a3093e617711c12b0
Oops61rus <antodeni@rambler.ru>
2020-02-29 11:14:11 +0300

    second

--- js/user.js
+++
@@ -1,9 +0,0 @@
-function User(name, email, password) {
-  this.name = name;
-  this.email = email;
-  this.password = password;
-}
-
-let name = document.querySelector('#userName').value;
-let email = document.querySelector('#userEmail').value;
-let email = document.querySelector('#userPass').value;
